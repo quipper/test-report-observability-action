680a1f168ca09123c318d596f89cdee880267f7a
Kuya KOHARA <kuya.kohara@gmail.com>
2026-09-07 16:02:32 +0900

    Truncate multiline failure message (#753)
    * Truncate multiline failure message
    
    * Autofix (ts / generate)
    
    Auto-generated-by: update-generated-files-action; https://github.com/quipper/test-report-observability-action/actions/runs/34092126248
    
    ---------
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

---
+++ tests/sentry.test.ts
@@ -0,0 +1,21 @@
+import { describe, expect, test } from 'vitest'
+import { truncateFailureMessage } from '../src/sentry.js'
+
+describe('truncateFailureMessage', () => {
+  test.each([
+    { given: undefined, expected: undefined },
+    { given: '', expected: '' },
+    { given: 'Error: something wrong', expected: 'Error: something wrong' },
+    {
+      given: 'Error: something wrong\nSECOND LINE WILL BE TRUNCATED',
+      expected: 'Error: something wrong\n(truncated...)',
+    },
+    {
+      given: 'Error: something wrong\r\nSECOND LINE WILL BE TRUNCATED',
+      expected: 'Error: something wrong\n(truncated...)',
+    },
+  ])(`truncateFailureMessage($given) should output $expected`, ({ given, expected }) => {
+    console.log('%o %o %o', given, expected, truncateFailureMessage(given))
+    expect(truncateFailureMessage(given)).toEqual(expected)
+  })
+})
